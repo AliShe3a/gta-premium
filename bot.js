89e70ae92500a57ad46f2c860d8e6efceb2ce6ba
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-12 21:37:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,18 +6,36 @@ const Enmap = require('enmap');
 const ytScraper = require("yt-scraper")
 const prefix = "!"
 
-client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
+client.on('ready', () => {
+  console.log(`Hixxx online`);
 
+});
+//
 client.on('message', message => {
-    if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('!obc')){
-if(!message.author.id === '490404894071717930') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
+if (message.content.split(' ')[0] == '!bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("CONNECT"))  return;
+member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+
+//ق محفوظه لهيكس وخبز
+client.on("message", message => {
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "!obc")) {
+                         if (!message.member.hasPermission("CONNECT"))  return;
+                            let embed4 = new Discord.RichEmbed()
+             .setDescription("**:white_check_mark: | جاري ارسال البرودكاست**")
+           .addField("مرسل البرودكاست" , message.author)
+          .addField("نص البرودكاست" ,args.join("  "))
+                            .addField("عدد الاعضاء المرسل لهم :busts_in_silhouette:" ,` **[${message.guild.memberCount}]**`,true)
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            
+                          }
 });
 
 client.on('message', message => {
